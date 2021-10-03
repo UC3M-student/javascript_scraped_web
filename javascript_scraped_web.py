f6e4db84f03bfa8836cc90d5772b354da1df995b
vitimerida <81519967+vitimerida@users.noreply.github.com>
2021-10-04 00:01:05 +0200

    Create javascript_scraped_web.py

---
+++ javascript_scraped_web.py
@@ -0,0 +1,29 @@
+from helium import *
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+url = "https://www.carrefour.es/"
+
+def get_data(url_sample):
+    Browser = start_chrome(url, headless=True)
+    browser_ = Browser.page_source
+    soup  = BeautifulSoup(browser_,"html.parser")
+    return soup
+
+def link_library(soup):
+    href_list = []
+    hrefs = soup.find_all("a", {"class":"text-banner__link track-click"})
+    w = "https://www.carrefour.es"
+    for i in hrefs:
+        i = i.get("href")
+        print(i)
+        href_list.append(i)
+    print(href_list)
+    href_list_remove = []
+    for y in href_list:
+        href_list_remove.append(y.replace(w,""))
+    perfect_href_list = []
+    for adds in href_list_remove:
+        perfect_href_list.append(w + adds)
+    return perfect_href_list
